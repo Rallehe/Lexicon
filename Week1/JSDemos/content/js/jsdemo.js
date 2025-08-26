e939dc7ed9365dc30c94aaa500ed0b0b3eb93249
Rasmus Hedin <rasmushedin@live.com>
2025-08-26 15:45:11 +0200

    Going through more basic JS

---
+++ Week1/JSDemos/content/js/jsdemo.js
@@ -0,0 +1,50 @@
+
+let x = 100;
+
+if (x === 100) {
+    console.log("Always use '===' when comparing numbers");
+}
+
+x = '100';
+
+if (x == 100) {
+    console.log("x is String but we compare to number with '==' which makes it true either way");
+}
+
+if (x != 100) {
+    console.log("Should be false as above is true");
+    console.log("Although should be true as x is not a number")
+}
+
+x < 50 ? console.log("It's less than 50") : console.log("It's bigger than 50!");
+
+let day = 24;
+
+if (day % 2 === 0) {
+    console.log("You can park!");
+}
+else {
+    console.log("Sorry no parking today");
+}
+
+day % 2 === 0 ? console.log("You can park") : console.log("You can't park");
+
+
+let f = [1,2,3,4,5,6,7,8,9];
+
+f.forEach(f => console.log(f));
+
+
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+console.log();
+
+for (let i = 56; i >= 22 ; i--) {
+    console.log(i);
+}
+
+
