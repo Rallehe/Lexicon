e5c60347ffc6406641adbd5b0f7f3cabbb000f4a
Rallehe <Rasmushedin@live.com>
2025-09-17 09:49:42 +0200

    Added error handeling to store assignment

--- Assignments/store.js
+++ Assignments/store.js
@@ -12,51 +12,76 @@ while (products.size < 5) {
     addProducts(input);
 }
 
-// Enter price to each product and store to Map (TODO: Safety check for number)
-products.forEach(product => {
-    console.log(`Enter price for ${product}: `);
-    let price = prompt();
-    productsMap.set(product, price);
-});
+enterProductPrice();
 
-while (true) {
-    productsMap.forEach((value, key) => {
-        console.log(`${key}: ${value}`);
-    });
-    console.log("What product do you which to purchase? Type 'exit' to finish purchase");
-    let input = prompt();
-    if (input.toLocaleLowerCase() == "exit") break;
-    else {
-        total += purchaseProduct(input, productsMap, totalPurchaseMap);
-    }
-}
+purchaseProducts();
 
 printPurchaseResult();
 
-function printPurchaseResult() {
-    console.log(`All your purchases`);
-    totalPurchaseMap.forEach((value, key) => {
-        console.log(`${key}: ${value}`);
+function purchaseProducts() {
+    while (true) {
+        productsMap.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
+        console.log("What product do you which to purchase? Type 'exit' to finish purchase");
+        let input = prompt();
+        if (input.toLocaleLowerCase() == "exit") break;
+        else {
+            total += purchaseProduct(input, productsMap, totalPurchaseMap);
+        }
+    }
+}
+
+function enterProductPrice() {
+    products.forEach(product => {
+        let price;
+        while (true) {
+            console.log(`Enter price for ${product}: `);
+            price = Number(prompt());
+            if (!isNaN(price) && price > 0) {
+                break;
+            }
+            console.log("Please enter a valid number greater than 0");
+        }
+        productsMap.set(product, price);
     });
+}
 
-    //Sort map in to array with most expensive item first.
-    const sortedPurchase = [...totalPurchaseMap.entries()].sort((a, b) => b[1] - a[1]);
-    const [key, value] = sortedPurchase[0];
+function printPurchaseResult() {
+    if (totalPurchaseMap.size > 0) {
+        console.log(`All your purchases`);
+        totalPurchaseMap.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
 
-    console.log(`Most expensive purchase: ${key}, ${value}`);
-    console.log("Total: " + total);
+        //Sort map in to array with most expensive item first.
+        const sortedPurchase = [...totalPurchaseMap.entries()].sort((a, b) => b[1] - a[1]);
+        const [expensiveKey, expensiveValue] = sortedPurchase[0];
+        const [cheapKey, cheapValue] = sortedPurchase[sortedPurchase.length - 1];
+
+        console.log(`Most expensive purchase: ${expensiveKey}: ${expensiveValue}`);
+        console.log(`Least expensive purchase: ${cheapKey}: ${cheapValue}`);
+        console.log("Total: " + total);
+    }
 }
 
 function purchaseProduct(input, productsMap, totalPurchaseMap) {
     let productPrice = productsMap.get(input);
 
-    if (productPrice === undefined) {
+    if (productsMap.get(input) === undefined) {
         console.log(`${input} is not a valid product.`);
-        return 0;
+        return;
     }
 
-    console.log(`How many ${input} do you wish to purchase?`);
-    let amount = Number(prompt());
+    let amount;
+    while (true) {
+        console.log(`How many ${input} do you wish to purchase?`);
+        amount = Number(prompt());
+        if (!isNaN(amount) && amount > 0) {
+            break;
+        }
+        console.log("Please enter a valid amount greater than 0");
+    }
 
     let cost = productPrice * amount;
 
