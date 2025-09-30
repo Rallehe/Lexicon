2a22e729faebecba9c2042b2c79f00108caeec04
Rallehe <Rasmushedin@live.com>
2025-09-30 07:28:13 +0200

    Typescript types

---
+++ Typescript/src/main.ts
@@ -0,0 +1,21 @@
+// npm run start
+
+
+
+
+// let myString: string = "String";
+// let myNumber: number = 42;
+// let isBoolean: boolean = true;
+// let myNull: null = null;
+// let myUndefined: undefined = undefined;
+// let myAny: any = "42";
+// let myUnknown: unknown = "UNKNOWN";
+
+// const myString2 = "String";
+// const test = "test" as const;
+
+
+// if (typeof myUnknown === "string") {
+//     console.log(myUnknown.toLowerCase());
+// }
+
