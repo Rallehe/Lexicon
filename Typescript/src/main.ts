b8a8a5d7b0fc428e42b6fb6f7f5a77d53eb3208c
Rallehe <Rasmushedin@live.com>
2025-10-01 19:15:46 +0200

    Objects, types and interface

--- Typescript/src/main.ts
+++ Typescript/src/main.ts
@@ -55,8 +55,8 @@
 // console.log(5/0);
 
 
-
-let role: "user" | "staff" | "admin";
+// type Role = "user" | "staff" | "admin";
+// let role: Role = "admin";
 
 // function doStuff(role: "user" | "staff" | "admin") {
 //     if (role === "admin") {
@@ -90,5 +90,122 @@ let role: "user" | "staff" | "admin";
 // console.log(myAge);
 
 // Force color to start the string with: '#' 
-let color: `#${string}` = '#8f2b2bff';
-let rgbColor: `rgb(${number},${number},${number})` = "rgb(255, 255, 255)";
+// let color: `#${string}` = '#8f2b2bff';
+// let rgbColor: `rgb(${number},${number},${number})` = "rgb(255, 255, 255)";
+
+type StringSize = "xs" | "sm" | "md" | "lg" | "xl";
+type NumberSize = 1 | 2 | 3 | 4 | 5;
+type Size = StringSize | NumberSize;
+
+type ID = string | number;
+type Color = "Red" | "Green" | "Blue";
+
+let id: ID = 1;
+let stringSize: StringSize = "xl";
+let numberSize: NumberSize = 4;
+let size = "4";
+
+
+
+type Person = {
+    name: string,
+    age: number,
+    // speak: () => void;
+    speak(): void;
+}
+
+type Employee = Person & {
+    salary: number,
+}
+
+interface Person2 {
+    name: string,
+    age: number,
+    speak(): void,
+}
+
+interface Employee2 extends Person2 {
+    salary: number,
+}
+
+let employee2: Employee2 = {
+    name: "Rasmus",
+    age: 27,
+    salary: 3000,
+    speak() {
+        console.log("Interface");
+    },
+}
+
+function speak(person: Person) {
+    person.speak();
+}
+
+let person1 = {
+    name: "Rasmus",
+    age: 27,
+    speak() {
+        console.log("AAAAHHHHH!");
+    },
+    // speak2: () => {
+    //     this;
+    // }
+};
+
+let person2: Person = {
+    name: "Oscar",
+    age: 30,
+    speak() {
+        console.log("Hey! My name is " + this.name);
+    }
+}
+
+speak(person1);
+speak(person2);
+
+let employee: Employee = {
+    name: "Rasmus",
+    age: 27,
+    salary: 50000,
+    speak() {
+        console.log(`${this.name} is an employee with ${this.salary} salary`);
+    },
+}
+
+speak(employee);
+
+
+type Car = {
+    readonly make: string;
+    readonly model: string;
+    owner?: Person; // No need to add owner when creating the car object "?"
+}
+
+const car: Car = {
+    make: "Volvo",
+    model: "740",
+    owner: person1,
+    // owner: "Rasmus",
+}
+
+// car.make = "Lexus";
+car.owner = person2;
+console.log(car)
+
+
+
+const myArray: string[] = ["A", "B", "C"];
+const myArray2 = ["A", "B", "C"];
+
+const myArray3 = [];
+
+
+
+
+
+// function crash(): never {
+//     throw new Error();
+// }
+// crash();
+// console.log("This will never run");
+
