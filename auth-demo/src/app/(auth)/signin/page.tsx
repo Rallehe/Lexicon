6d724b4d88e5174fc23b789a6802d0f32c382c9d
Rallehe <Rasmushedin@live.com>
2025-11-24 15:20:46 +0100

    Adding form for signin/up and spinner while loading

--- auth-demo/src/app/(auth)/signin/page.tsx
+++ auth-demo/src/app/(auth)/signin/page.tsx
@@ -1,21 +1,89 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { authClient } from "@/lib/auth-client";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+
+
+
+const formSchema = z.object({
+    email: z.email(),
+    password: z.string().min(8, "Password must be at least 8 characters")
+});
+
 
 export default function Page() {
-    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    })
+
+    async function onSubmit(formData: z.infer<typeof formSchema>) {
+        setIsLoading(true);
+        await authClient.signIn.email({
+            email: formData.email,
+            password: formData.password,
+            callbackURL: "/",
+        });
+        setIsLoading(false);
+    }
+
     return (
-        <Button
-            onClick={async () => {
-                await authClient.signIn.email({
-                    email: "Rasmus@example.se",
-                    password: "Lexicon123",
-                });
-                router.push("/")
-            }}>
-            Sign In
-        </Button>
+        <div className="flex items-center justify-center">
+            <div className="w-full max-w-md">
+                <form id="signin-form" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup>
+                        <Controller
+                            name="email"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor="add-user-email">Email</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id="add-user-email"
+                                        aria-invalid={fieldState.invalid}
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor="add-user-password">Password</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id="add-user-password"
+                                        aria-invalid={fieldState.invalid}
+                                        type="password"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                    </FieldGroup>
+                    <div className="mt-4">
+                        <Button type="submit" form="signin-form">Sign In {isLoading ? <Spinner /> : ""}</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
--- auth-demo/src/app/(auth)/signup/page.tsx
+++ auth-demo/src/app/(auth)/signup/page.tsx
@@ -1,20 +1,104 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { authClient } from "@/lib/auth-client";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+const formSchema = z.object({
+    name: z.string().min(1, "Name must be at least one character"),
+    email: z.email(),
+    password: z.string().min(8, "Password must be at least 8 characters")
+});
+
+
+
 
 export default function Page() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        }
+    })
+
+    async function onSubmit(formData: z.infer<typeof formSchema>) {
+        await authClient.signUp.email({
+            name: formData.name,
+            email: formData.email,
+            password: formData.password,
+            callbackURL: "/",
+        });
+    }
     return (
-        <Button
-            onClick={async () => {
-                await authClient.signUp.email({
-                    name: "Rasmus",
-                    email: "Rasmus@example.se",
-                    password: "Lexicon123",
-                    
-                })
-            }}>
-            Sign Up
-        </Button>
+        <div className="flex items-center justify-center">
+            <div className="w-full max-w-md">
+                <form id="signup-form" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup>
+                        <Controller
+                            name="name"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor="add-user-name">Name</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id="add-user-name"
+                                        aria-invalid={fieldState.invalid}
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                        <Controller
+                            name="email"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor="add-user-email">Email</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id="add-user-email"
+                                        aria-invalid={fieldState.invalid}
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor="add-user-password">Password</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id="add-user-password"
+                                        aria-invalid={fieldState.invalid}
+                                        type="password"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                    </FieldGroup>
+                    <div className="mt-4">
+                        <Button type="submit" form="signup-form">Sign Up</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
